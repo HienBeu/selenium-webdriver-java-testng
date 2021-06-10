f1e77d233d9913428c25e6d0a413edd80820f0e8
Admin <Admin@DESKTOP-342LQV4>
2021-06-10 11:30:18 +0700

    abc

--- selenium/Webdriver/Topic_06_Web_Brower_Excecise.java
+++ selenium/Webdriver/Topic_06_Web_Brower_Excecise.java
@@ -1,4 +1,4 @@
-package Webdriver;
+ package Webdriver;
 
 import java.util.concurrent.TimeUnit;
 
---
+++ selenium/Webdriver/Topic_07_Web_Element_Excecise.java
@@ -0,0 +1,81 @@
+package Webdriver;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_07_Web_Element_Excecise {
+	// Khai báo biến (Declare)
+	WebDriver driver;
+	String firstName, lastName, emailAddress, passWord;
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://live.demoguru99.com/");
+
+		// Khởi tạo data test
+		firstName = "Osama";
+		lastName = "Bin Laden";
+		emailAddress = "osama" + generateEmail();
+		passWord = "123456";
+	}
+
+	@Test
+	public void TC_01_Creat_New_account() {
+		driver.get("http://live.demoguru99.com/");
+
+		driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
+
+		driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+
+		driver.findElement(By.id("firstname")).sendKeys(firstName);
+
+		driver.findElement(By.id("lastname")).sendKeys(lastName);
+
+		driver.findElement(By.id("email_address")).sendKeys(emailAddress);
+
+		driver.findElement(By.id("password")).sendKeys(passWord);
+
+		driver.findElement(By.id("confirmation")).sendKeys(passWord);
+
+		driver.findElement(By.xpath("//button[@title='Register']")).click();
+		
+		Assert.assertEquals(driver.findElement(By.xpath("//li[@class='success-msg']//span")).getText(), "Thank you for registering with Main Website Store.");
+		
+		// Dùng hàm isDisplay để kiểm tra
+		Assert.assertEquals(By.xpath("//h3[text()= 'Contact Information']/parent::div/following-sibling::div/p[contains(String(),'daoconhoa@gmail.com')]")).isDisplay;
+	}
+
+	@Test
+	public void TC_01_Classname() {
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+	
+	public String generateEmail() {
+		Random rand = new Random();
+		return rand.nextInt(9999) + "@email.vn";
+	}
+	
+	public void sleepInSecond(long timeoutInSecond) {
+		try {
+		   Thread.sleep(timeoutInSecond * 1000);
+		} catch (InterruptedException e) {
+		   e.printStackTrace();
+		}
+	}
+
+}
